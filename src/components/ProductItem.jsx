71f120600d3064b641823a9552f265baae28e388
pickupbizadmin <admin@pickupbiz.in>
2023-10-05 11:17:04 +0530

    Initial Commit

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const ProductItem=()=>{
+    return(
+        <h1>ProductItem</h1>
+    )
+}
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const ProductList=()=>{
+    return(
+        <h1>ProductList</h1>
+    )
+}
